af7c106c2cda1394572c7181ebac2b444e9335d7
prax93 <prashanthsritharan@Prashanths-MacBook-Pro-2.local>
2023-12-18 12:48:07 +0100

    Added Factory Class MovieService into Controller

--- Controller/controller.js
+++ Controller/controller.js
@@ -1,14 +1,15 @@
-import getMovies from '../Service/service.js'; 
+import MovieService from '../Service/service.js';
 import express from 'express';
 
 
 const app = express()
 const port = 3000
+const moviesService = new MovieService();
 
 app.get('/movies', async (req, res) => {
 
   try {
-    let movies = await getMovies()
+    let movies = await moviesService.getMovies()
     if (movies instanceof Error) {
       throw movies; // Jump to the catch block
     }
