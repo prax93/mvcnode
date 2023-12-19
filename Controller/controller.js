ee05876a5139bc39b5928c5e0c5656fd863dacbc
Prashanth Sritharan <prashanth.sritharan@sbb.ch>
2023-12-19 14:57:05 +0100

    Implemented get movie by ID Endpoint

--- Controller/controller.js
+++ Controller/controller.js
@@ -2,7 +2,11 @@ import MovieService from '../Service/service.js';
 import express from 'express';
 
 const app = express()
-const port = 3000
+const port = 80
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 app.get('/movies', async (req, res) => {
 
@@ -21,6 +25,23 @@ app.get('/movies', async (req, res) => {
   
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get('/:id', async (req, res) => {
+
+  var id = req.params.id;
+  var test = req.params.test
+  const moviesService = new MovieService();
+  
+
+  try {
+    let movie = await moviesService.getMovie(id)
+    if (movie instanceof Error) {
+      throw movie; // Jump to the catch block
+    }
+    res.send(movie)
+  } catch (error) {
+    res.status(404).send(error.message)
+    
+  }
+
 })
+
